cd106973b9c3839af9fd266a209479a703b733f1
wilder <henno.ambre@gmail.com>
2023-07-20 09:44:57 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from '../src/components/HomePage'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+    <Routes>
+          <Route path="/" element={<HomePage />} /> 
+        </Routes>
+    </div>
+  )
+}
+
+export default App
