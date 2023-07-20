cd49d9de4eb3d99f6a1fd7a6e1748165b569a04e
wilder <henno.ambre@gmail.com>
2023-07-20 16:49:28 +0200

    good version

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,6 @@ import "../components/HomePageStyle.scss"
 import backgroundVideo from "../assets/tarantino-video.mp4";
 import NavBar from './NavBar'; 
 
-
 function HomePage() {
   return (
     <div className="home-page">
@@ -13,8 +12,10 @@ function HomePage() {
           <source src={backgroundVideo} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
+      </div>   
+      <div className="title-name-project">
+        <h1>Tarantino`s <br /> Project</h1>
       </div>
-      
     </div>
   );
 }
